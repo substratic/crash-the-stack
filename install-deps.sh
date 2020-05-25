d957c2bb4099d3022fae33d35619e42c63325b5e
David Wilson <david@daviwil.com>
2020-05-25 15:51:30 -0700

    Add ~~bin to install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -3,6 +3,6 @@
 SUBSTRATIC_VERSION=
 
 # Ensure substratic modules are installed
-[[ ! -d lib/github.com/substratic/sdl2 ]] && ./gambit/bin/gsi -:~~lib=./gambit/lib,~~userlib=./lib -install github.com/substratic/sdl2
-[[ ! -d lib/github.com/substratic/build ]] && ./gambit/bin/gsi -:~~lib=./gambit/lib,~~userlib=./lib -install github.com/substratic/build
-[[ ! -d lib/github.com/substratic/engine ]] && ./gambit/bin/gsi -:~~lib=./gambit/lib,~~userlib=./lib -install github.com/substratic/engine
+[[ ! -d lib/github.com/substratic/sdl2 ]] && ./gambit/bin/gsi -:~~bin=./gambit/bin,~~lib=./gambit/lib,~~userlib=./lib -install github.com/substratic/sdl2
+[[ ! -d lib/github.com/substratic/build ]] && ./gambit/bin/gsi -:~~bin=./gambit/bin,~~lib=./gambit/lib,~~userlib=./lib -install github.com/substratic/build
+[[ ! -d lib/github.com/substratic/engine ]] && ./gambit/bin/gsi -:~~bin=./gambit/bin,~~lib=./gambit/lib,~~userlib=./lib -install github.com/substratic/engine
