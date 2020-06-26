a003529b3d56a2520fff7bc3a268afe79a54a7a5
David Wilson <david@daviwil.com>
2020-06-26 06:28:37 -0700

    Remove unneeded config from ./dev.sh, increase debug level to 3

--- dev.sh
+++ dev.sh
@@ -3,6 +3,4 @@
 # Ensure dependencies
 . ./install-deps.sh
 
-export DYLD_FRAMEWORK_PATH="$(pwd)/lib/macos"
-
-./gambit/bin/gsi -:dar,~~bin=./gambit/bin,~~include=./gambit/include,~~lib=./gambit/lib,~~userlib=./lib src/ build.scm --dev "$@"
+./gambit/bin/gsi -:d3ar,~~userlib=./lib src/ build.scm --dev "$@"
